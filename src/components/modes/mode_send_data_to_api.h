baf11f606fec328f52df4743ef815ffc914cfd21
indexGuilherme <gddcruz04@gmail.com>
2022-11-17 13:48:55 -0300

    começando a controlar o motor

--- src/components/modes/mode_send_data_to_api.h
+++ src/components/modes/mode_send_data_to_api.h
@@ -1,15 +1,19 @@
 #ifndef _MODE_SEND_DATA_TO_API_
 #define _MODE_SEND_DATA_TO_API_
 
+#include <Arduino.h>
 #include "./mode_basic_sample.h"
 
 class ModeSendDataToApi : public ModeBasicSample{
+
+    private:
+        const int stepsPerRevolution = 500;
 
     public:
         ModeSendDataToApi(){}
 
         void loop() {
-            
+            Serial.println("teste");
         }
 
 };
--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,8 @@
 #include "components/modes/mode_basic_sample.h"
 #include "components/modes/mode_configuration.h"
 #include "components/modes/mode_create_yourself_in_database.h"
+#include "components/modes/mode_send_data_to_api.h"
 #include "utils/custom_wifi.h"
-
-const int stepsPerRevolution = 500;
 
 #define IN1 13
 #define IN2 12
@@ -28,6 +27,7 @@ void setup()
     Serial.begin(115200);
 
     NonVolatileStorage nonVolatileStorage;
+    nonVolatileStorage.write(Settings("id", "name", "description", "user_id", 3, "ssid", "password"));
     JsonTools jsonTools;
 
     if (!nonVolatileStorage.check("name"))
@@ -38,6 +38,8 @@ void setup()
     {
         if(!nonVolatileStorage.check("id")) {
             mode = new ModeCreateYourselfInDatabase(&nonVolatileStorage, &jsonTools);
+        } else {
+            mode = new ModeSendDataToApi();
         }
     }
 }
